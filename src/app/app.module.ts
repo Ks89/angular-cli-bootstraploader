f7f6d7845250157346a9104568297366f32471b5
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-08-11 14:07:11 +0200

    working example of bootstrap-loader + bootstrap alpha 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Third party libraries that are using scss/css
+import 'bootstrap-loader';
+
 @NgModule({
   declarations: [
     AppComponent
